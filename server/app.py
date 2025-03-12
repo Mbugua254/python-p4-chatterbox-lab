480cade546e360d65d81afb65d53ad87289d1301
Michael Mbugua <mikembugua254@gmail.com>
2025-03-12 11:54:55 +0300

    Created an API with Flask for a React Frontent Application

--- server/app.py
+++ server/app.py
@@ -14,13 +14,43 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+#Get all messages
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+#Create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+
+# Route to update a message by ID
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message, id) or abort(404)
+    data = request.get_json()
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+# Route to delete a message by ID
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id) or abort(404)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
